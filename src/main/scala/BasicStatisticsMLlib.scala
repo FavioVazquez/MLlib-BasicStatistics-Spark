190b27b3ae6db567b70fd5e0174048c4e6e96f2e
FavioVazquez <favio.vazquezp@gmail.com>
2015-05-18 11:38:43 -0430

    - Updated skeleton

--- src/main/scala/BasicStatisticsMLlib.scala
+++ src/main/scala/BasicStatisticsMLlib.scala
@@ -1,6 +1,20 @@
+import org.apache.spark.{SparkConf, SparkContext}
+
 /**
  * Created by worker1 on 18/05/15.
  */
 object BasicStatisticsMLlib {
 
+def main(args: Array[String]) {
+
+  val conf = new SparkConf()
+    //  .setMaster("local")
+    .setMaster("mesos://master.mcbo.mood.com.ve:5050")
+    .setAppName("Basic Statistics MLlib")
+    .set("spark.executor.memory", "12g")
+  val sc = new SparkContext(conf)
+
+  sc.stop()
+  }
 }
+
