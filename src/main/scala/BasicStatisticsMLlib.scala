5b4db58950b7db794ceb0aca203b5bade051f845
FavioVazquez <favio.vazquezp@gmail.com>
2015-05-18 12:04:05 -0430

    - Started section 1

--- src/main/scala/BasicStatisticsMLlib.scala
+++ src/main/scala/BasicStatisticsMLlib.scala
@@ -1,3 +1,6 @@
+import org.apache.spark.mllib.linalg.{Vector, Vectors}
+import org.apache.spark.mllib.stat.{Statistics, MultivariateStatisticalSummary}
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
@@ -8,11 +11,33 @@ object BasicStatisticsMLlib {
 def main(args: Array[String]) {
 
   val conf = new SparkConf()
-    //  .setMaster("local")
-    .setMaster("mesos://master.mcbo.mood.com.ve:5050")
+      .setMaster("local")
+//    .setMaster("mesos://master.mcbo.mood.com.ve:5050")
     .setAppName("Basic Statistics MLlib")
     .set("spark.executor.memory", "12g")
   val sc = new SparkContext(conf)
+
+//  1. Summary Statistics
+
+  /**
+   * We provide column summary statistics for RDD[Vector] through the
+   * function colStats available in Statistics.
+   */
+
+  /**
+   * colStats() returns an instance of MultivariateStatisticalSummary,
+   * which contains the column-wise max, min, mean, variance, and number
+   * of nonzeros, as well as the total count.
+   */
+
+  val observations: RDD[Vector] = sc.parallelize(Seq.range(0.0,100.0)
+  .map {i => Vectors.dense(i) } )
+
+  // Compute column summary statistics.
+  val summary: MultivariateStatisticalSummary = Statistics.colStats(observations)
+  println(summary.mean) // a dense vector containing the mean for each column
+  println(summary.variance) // column-wise variance
+  println(summary.numNonzeros) //number of nonzeros in each column
 
   sc.stop()
   }
