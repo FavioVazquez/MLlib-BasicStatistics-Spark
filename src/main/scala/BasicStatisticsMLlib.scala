f9466c226a2dbdddfd9eb9c7c0d5ebeb48390a1b
FavioVazquez <favio.vazquezp@gmail.com>
2015-05-18 14:01:50 -0430

    - Tries with local master fine - Tries with cluster master fine

--- src/main/scala/BasicStatisticsMLlib.scala
+++ src/main/scala/BasicStatisticsMLlib.scala
@@ -11,8 +11,8 @@ object BasicStatisticsMLlib {
 def main(args: Array[String]) {
 
   val conf = new SparkConf()
-      .setMaster("local")
-//    .setMaster("mesos://master.mcbo.mood.com.ve:5050")
+//      .setMaster("local")
+    .setMaster("mesos://master.mcbo.mood.com.ve:5050")
     .setAppName("Basic Statistics MLlib")
     .set("spark.executor.memory", "12g")
   val sc = new SparkContext(conf)
