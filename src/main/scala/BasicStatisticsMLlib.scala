93b8461a62b530a9259840a18ea812829ab2e456
FavioVazquez <favio.vazquezp@gmail.com>
2015-05-18 14:00:56 -0430

    - Finish tutorial by now (return with real data to finish other sections) - Try with local master

--- src/main/scala/BasicStatisticsMLlib.scala
+++ src/main/scala/BasicStatisticsMLlib.scala
@@ -17,27 +17,27 @@ def main(args: Array[String]) {
     .set("spark.executor.memory", "12g")
   val sc = new SparkContext(conf)
 
-////  1. Summary Statistics
-//
-//  /**
-//   * We provide column summary statistics for RDD[Vector] through the
-//   * function colStats available in Statistics.
-//   */
-//
-//  /**
-//   * colStats() returns an instance of MultivariateStatisticalSummary,
-//   * which contains the column-wise max, min, mean, variance, and number
-//   * of nonzeros, as well as the total count.
-//   */
-//
-//  val observations: RDD[Vector] = sc.parallelize((0 to 100).toSeq
-//  .map {i => Vectors.dense(i) } )
-//
-//  // Compute column summary statistics.
-//  val summary: MultivariateStatisticalSummary = Statistics.colStats(observations)
-//  println(summary.mean) // a dense vector containing the mean for each column
-//  println(summary.variance) // column-wise variance
-//  println(summary.numNonzeros) //number of nonzeros in each column
+//  1. Summary Statistics
+
+  /**
+   * We provide column summary statistics for RDD[Vector] through the
+   * function colStats available in Statistics.
+   */
+
+  /**
+   * colStats() returns an instance of MultivariateStatisticalSummary,
+   * which contains the column-wise max, min, mean, variance, and number
+   * of nonzeros, as well as the total count.
+   */
+
+  val observations: RDD[Vector] = sc.parallelize((0 to 100).toSeq
+  .map {i => Vectors.dense(i) } )
+
+  // Compute column summary statistics.
+  val summary: MultivariateStatisticalSummary = Statistics.colStats(observations)
+  println(summary.mean) // a dense vector containing the mean for each column
+  println(summary.variance) // column-wise variance
+  println(summary.numNonzeros) //number of nonzeros in each column
 
 //  2. Correlations
 
@@ -49,22 +49,22 @@ def main(args: Array[String]) {
    * correlation.
    */
 
-//  val seriesX: RDD[Double] = sc.parallelize(Seq.fill(1000)(math.random)
-//  . map { i => i})
-//  seriesX.take(20).foreach(println)
-//
-//  val seriesY: RDD[Double] = sc.parallelize(Seq.fill(1000)(math.random)
-//  .map { i => i})
-//  seriesY.take(20).foreach(println)
-//
-//    /**
-//     * compute the correlation using Pearson's method. Enter "spearman"
-//     * for Spearman's method. If a method is not specified,
-//     * Pearson's method will be used by default.
-//     */
-//
-//  val correlation: Double = Statistics.corr(seriesX,seriesY,"pearson")
-//  println(correlation)
+  val seriesX: RDD[Double] = sc.parallelize(Seq.fill(1000)(math.random)
+  . map { i => i})
+  seriesX.take(20).foreach(println)
+
+  val seriesY: RDD[Double] = sc.parallelize(Seq.fill(1000)(math.random)
+  .map { i => i})
+  seriesY.take(20).foreach(println)
+
+    /**
+     * compute the correlation using Pearson's method. Enter "spearman"
+     * for Spearman's method. If a method is not specified,
+     * Pearson's method will be used by default.
+     */
+
+  val correlation: Double = Statistics.corr(seriesX,seriesY,"pearson")
+  println(correlation)
 
   val data: RDD[Vector] = sc.parallelize(Seq.fill(100)(math.random)
   .map {i => Vectors.dense(i)})
@@ -79,6 +79,7 @@ def main(args: Array[String]) {
   val correlMatrix: Matrix = Statistics.corr(data,"pearson")
   println(correlMatrix)
 
+  // TODO: Finish tutorial with real data
   sc.stop()
   }
 }
